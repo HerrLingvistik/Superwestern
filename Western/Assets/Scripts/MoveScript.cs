45ae10de5bddc6435726134b3c2b0c510c132796
Herr Lingvistik <gusha495@student.liu.se>
2015-09-21 13:19:44 +0200

    yo brush

---
+++ Western/Assets/Scripts/MoveScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Player controller
+/// </summary>
+public class MoveScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
